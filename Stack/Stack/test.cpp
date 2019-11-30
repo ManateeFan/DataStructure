126bf2f2bee35877a7524e21a6092e91af1505f4
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-11-30 19:40:01 +0800

    Update Stack

---
+++ Stack/Stack/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Stack.hpp"
+
+using namespace std;
+
+int main()
+{
+	Stack<int, 20> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	cout << "s is empty: " << s.empty() << endl;
+	cout << "s.top: " << s.top() << endl;
+	s.pop();
+	cout << "s.pop() - > s.top: " << s.top() << endl;
+	return 0;
+}
