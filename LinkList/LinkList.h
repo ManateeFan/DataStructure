3ec121ae4eaaea6e6bb7a48add0a898ce062b598
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-11-30 23:04:48 +0800

    LinkList

---
+++ LinkList/LinkList.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <stdexcept>
+template<typename Key>
+struct Node
+{
+	using pointer = Node<Key>*;
+	Key key;
+	Node* prev;
+	Node* next;
+	Node(Key const& k) :key(k), prev(nullptr), next(nullptr) {}
+};
+
+template<typename T>
+class LinkList
+{
+private:
+	typename Node<T>::pointer head;
+
+public:
+	LinkList() :head(nullptr) {};
+
+	// 查找第一个关键字为k的元素并返回指针
+	typename Node<T>::pointer Search(T const&) const;
+
+	// 将节点x插入LinkList前端
+	void Insert( typename Node<T>::pointer const& x);
+
+	// 删除节点x
+	void Delete(typename Node<T>::pointer const& x);
+};
+
+template<typename T>
+inline 
+typename Node<T>::pointer LinkList<T>::Search(T const& k) const
+{
+	if (!head)
+		throw std::out_of_range("LinkList is empty");
+	typename Node<T>::pointer p = head;
+	while (p->key != k)
+	{
+		p = p->next;
+	}
+	return p;
+}
+
+template<typename T>
+inline
+void LinkList<T>::Insert(typename Node<T>::pointer const& x)
+{
+	x.next = head;
+	if (head)
+		head->prev = x;
+	head = x;
+	x->prev = nullptr;
+}
+
+template<typename T>
+inline
+void LinkList<T>::Delete(typename Node<T>::pointer const& x)
+{
+	if (!x->prev)
+		x->prev->next = x->next;
+	else
+		head->next = x->next;
+	if (!x->next)
+		x->next->prev = x->prev;
+}
---
+++ LinkList/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "LinkList.h"
+
+
+int main()
+{
+	return 0;
+}
