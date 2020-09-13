008fa1c992ef2b3b6efe04187c443092ebe2f68d
Yifan Liu <43963430+ManateeFan@users.noreply.github.com>
2020-09-13 10:01:38 +0800

    Add selectionSort.cpp

--- Sort/selectionSort.cpp
+++ Sort/selectionSort.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 
 int main() {
-  std::vector<int> v{5, 4, 3, 2, 1};
+    std::vector<int> v{ 5, 4, 3, 2, 1 };
 
-  for (int i = 0; i < v.size() - 1; i++) {
-    int min = i;
-    for (int j = i + 1; j < v.size(); j++) {
-      if (v[i] > v[j]) min = j;
+    for (int i = 0; i < v.size() - 1; i++) 
+    {
+        int min = i;
+        for (int j = i + 1; j < v.size(); j++) 
+        {
+            if (v[i] > v[j]) min = j;
+        }
+        std::swap(v[i], v[min]);
     }
-    std::swap(v[i], v[min]);
-  }
 
-  for (auto num : v) std::cout << num << " ";
+    for (auto num : v) std::cout << num << " ";
 }
