885d7d7481650696ace34a87e3bc490e34456e48
Yifan Liu <43963430+ManateeFan@users.noreply.github.com>
2020-09-13 10:01:23 +0800

    Add insertionSort.cpp

--- Sort/insertionSort.cpp
+++ Sort/insertionSort.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-  std::vector<int> v{5, 4, 3, 2, 1};
+    std::vector<int> v{ 5, 4, 3, 2, 1 };
 
-  for (int i = 1; i < v.size(); i++)
-  {
-    for (int j = i; j >= 0 && v[j] < v[j - 1]; j--)
+    for (int i = 1; i < v.size(); i++)
     {
-      std::swap(v[j], v[j - 1]);
+        for (int j = i; j >= 0 && v[j] < v[j - 1]; j--)
+        {
+            std::swap(v[j], v[j - 1]);
+        }
     }
-  }
 
-  for (auto num : v)
-    std::cout << num << " ";
+    for (auto num : v)
+        std::cout << num << " ";
 }
